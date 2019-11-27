38b9d8ef7ad554f0ab1c6bdfeec667fb7c970506
pietro <pietrof@live.com>
2019-11-27 12:28:24 +0100

    1

--- sketch.js
+++ sketch.js
@@ -7,39 +7,52 @@ var op = 0;
 var opText = 255;
 var opImg = 0;
 
-function preload(){
+function preload() {
   sigla = loadSound('assets/TG1_bumper.mp3')
-  map = loadImage('assets/map.jpg')
   tg = loadImage('assets/tg1_logo_wh.png')
+  // tv = loadImage('assets/tv.png')
   font = loadFont('assets/pcsenior.ttf');
 
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight, WEBGL)
+  createCanvas(windowWidth, windowHeight, WEBGL)
 
   analyzer = new p5.Amplitude();
   analyzer.setInput(sigla);
 
   pixelDensity(0.9)
 
-  //vhs()
-
-
-
   //button to let the intro (and also sound) play
-    button = createButton("LET'S GET IT STARTED");
-    button.style('background-color', 'black');
-    button.style('color', 'gold')
-    button.style('padding', '15px');
-    button.style('font-family', font)
-    button.style('border', '5px solid gold');
-    button.position(windowWidth / 2-80, windowHeight / 2 + windowHeight / 10);
-    button.mousePressed(playAll);
+  button = createButton("LET'S GET IT STARTED");
+  button.style('background-color', 'black');
+  button.style('color', 'gold')
+  button.style('padding', '15px');
+  button.style('font-family', font)
+  button.style('border', '5px solid gold');
+  button.style('cursor', 'pointer')
+  button.position(windowWidth / 2 - 80, windowHeight / 2 + windowHeight / 10);
+  button.mousePressed(playAll);
+
+  restart = createButton("RESTART");
+  restart.hide()
+  restart.style('background-color', 'black');
+  restart.style('color', 'gold')
+  restart.style('padding', '15px');
+  restart.style('font-family', font)
+  restart.style('cursor', 'pointer')
+  restart.style('border', '5px solid gold');
+  restart.position(windowWidth / 2 - 50,windowHeight / 10 * 9);
+  restart.mousePressed(ricomincia);
+
 }
 
 function draw() {
-  background('black')
+  background('black');
+  imageMode(CENTER)
+
+  // image(tv,0,0,windowWidth,windowHeight)
+
   // text settigs and display
   textFont(font);
   textAlign(CENTER, CENTER);
@@ -51,41 +64,33 @@ function draw() {
   t2 = "TRIBUTE TO TG1 BUMPER"
   text(t2, 0, 20)
 
-  imageMode(CENTER)
-  tint(255,opImg)
-  image(tg,0,0,400,400/1.42)
+  var incr = analyzer.getLevel();
 
+  push()
+  rotateX(incr);
+  rotateZ(incr);
+  rotateY(incr);
+  translate(0,0,100+incr*500)
+  imageMode(CENTER)
+  tint(255, opImg)
+  image(tg, 0, 0, 400 + incr, 400 / 1.42 + incr)
+  pop()
 
   noFill();
-  stroke(255,255,255,op);
+  stroke(255, 255, 255, op);
   strokeWeight(1)
   push()
-  translate(windowWidth*0.5, height * 0.45, -200);
-  rotateX(frameCount*0.001)
-  rotateY(frameCount*0.001)
-  rotateZ(frameCount*0.001)
+  translate(windowWidth * 0.5, height * 0.45, -200);
+  rotateX(frameCount * 0.001)
+  rotateY(frameCount * 0.001)
+  rotateZ(frameCount * 0.001)
   sphere(600);
   pop();
 }
 
-function vhs() {
-  loadPixels(); //
-    for (var y=0; y< height; y++) {
-      for (var x=0; x< width; x++) {
-        var index = (x+y*width)*4;
-        pixels[index+0] = random()*200;
-        pixels[index+1] = random()*200;
-        pixels[index+2] = random()*200;
-        pixels[index+3] = random()*200;
-      }
-    }
-  updatePixels() //
-}
-
 function playAll() {
-
-
   button.hide()
+  restart.show()
 
   if (sigla.isPlaying()) {
     sigla.pause();
@@ -93,15 +98,22 @@ function playAll() {
     sigla.loop();
   }
 
+  t1 = " "
+  t2 = " "
   op = 50;
   opText = 0;
-  opImg = 255
-
-
-
-
+  opImg = 255;
 }
 
-function windowResized(){
+function ricomincia() {
+  op = 0;
+  opText = 255;
+  opImg = 0;
+  sigla.pause();
+  button.show()
+  restart.hide()
+}
+
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
